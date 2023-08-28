5d47de40be55701a29f004b308d75d34fd9d11b8
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-08-28 17:45:10 +0200

    full greeting message

--- src/scenes/greeting/message.tsx
+++ src/scenes/greeting/message.tsx
@@ -5,17 +5,33 @@ export function Message() {
   const [currentIndexFirstLine, setCurrentIndexFirstLine] = useState(0);
   const [currentTextSecondLine, setCurrentTextSecondLine] = useState("");
   const [currentIndexSecondLine, setCurrentIndexSecondLine] = useState(0);
+  const [firstPartWritten, setFirtsPartWritten] = useState(false);
+  const [firstPartDeleted, setFirstPartDeleted] = useState(false);
 
   const firstLine: string = "Hi, I'm Andrzej Kukuryk";
   const musician: string = "Musician";
-  const typingSpeed = Math.floor(Math.random() * 100) + 50;
-  useEffect(() => typeText(), [currentIndexFirstLine, currentIndexSecondLine]);
+  const frontend: string = "Frontend developer and musician";
+  const typingSpeed = () => {
+    if (currentIndexSecondLine === 18) {
+      return 1100;
+    }
+    return Math.floor(Math.random() * 80) + 50;
+  };
+  useEffect(
+    () => typeText(),
+    [
+      currentIndexFirstLine,
+      currentIndexSecondLine,
+      firstPartWritten,
+      firstPartDeleted,
+    ]
+  );
 
   const typeFirstLine = () => {
     if (currentIndexFirstLine < firstLine.length) {
       setTimeout(() => {
         setCurrentIndexFirstLine(currentIndexFirstLine + 1);
-      }, typingSpeed);
+      }, typingSpeed());
     }
   };
 
@@ -23,7 +39,29 @@ export function Message() {
     if (currentIndexSecondLine < musician.length) {
       setTimeout(() => {
         setCurrentIndexSecondLine(currentIndexSecondLine + 1);
-      }, typingSpeed);
+      }, typingSpeed());
+    } else {
+      setTimeout(() => {
+        setFirtsPartWritten(true);
+      }, 800);
+    }
+  };
+
+  const deleteSecondLine = () => {
+    if (currentIndexSecondLine > 0) {
+      setTimeout(() => {
+        setCurrentIndexSecondLine(currentIndexSecondLine - 1);
+      }, 50);
+    } else {
+      setFirstPartDeleted(true);
+    }
+  };
+
+  const typeSecondLineAgain = () => {
+    if (currentIndexSecondLine < frontend.length) {
+      setTimeout(() => {
+        setCurrentIndexSecondLine(currentIndexSecondLine + 1);
+      }, typingSpeed());
     }
   };
 
@@ -32,12 +70,30 @@ export function Message() {
     const typingFirstLine: string = firstLine.slice(0, currentIndexFirstLine);
     setCurrentTextFirstLine(typingFirstLine);
     if (currentIndexFirstLine === firstLine.length) {
-      typeSecondLine();
-      const typingSecondLine: string = musician.slice(
-        0,
-        currentIndexSecondLine
-      );
-      setCurrentTextSecondLine(typingSecondLine);
+      if (!firstPartWritten) {
+        typeSecondLine();
+        const typingSecondLine: string = musician.slice(
+          0,
+          currentIndexSecondLine
+        );
+        setCurrentTextSecondLine(typingSecondLine);
+      }
+      if (firstPartWritten && !firstPartDeleted) {
+        deleteSecondLine();
+        const deletingSecondLine: string = musician.slice(
+          0,
+          currentIndexSecondLine
+        );
+        setCurrentTextSecondLine(deletingSecondLine);
+      }
+      if (firstPartDeleted) {
+        typeSecondLineAgain();
+        const typingSecondLineAgain: string = frontend.slice(
+          0,
+          currentIndexSecondLine
+        );
+        setCurrentTextSecondLine(typingSecondLineAgain);
+      }
     }
   };
   return (
