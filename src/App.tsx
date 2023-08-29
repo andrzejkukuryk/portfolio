142f61b4587ed97b4f03a6d83d2499b55742e78d
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-08-29 17:54:53 +0200

    add stack icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Message } from "./scenes/greeting/message";
+import { StackItem } from "./scenes/greeting/stackItem";
 
 function App() {
   return (
     <div className="App">
       <Message />
+      <StackItem />
     </div>
   );
 }
