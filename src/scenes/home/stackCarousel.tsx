45ad7e862fb586b1f7e25aeb2aa80dfcc41f4ca9
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-09-21 10:10:51 +0200

    carousel slides

--- src/scenes/home/stackCarousel.tsx
+++ src/scenes/home/stackCarousel.tsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import { StackInfo, stack } from "../../data/stackIcons";
 import { StackItem } from "./stackItem";
 import styles from "./stackCarousel.module.scss";
+import classNames from "classnames";
 
 export function StackCarousel() {
   const [currentOrder, setCurrentOrder] = useState<StackInfo[]>(stack);
+  const [toLeft, setToLeft] = useState(false);
+  const [toRight, setToRight] = useState(false);
 
   console.log(currentOrder);
 
@@ -13,6 +16,7 @@ export function StackCarousel() {
     const firstItem = temporaryOrder.shift();
     if (firstItem) {
       temporaryOrder.push(firstItem);
+      setToLeft(false);
     }
     setCurrentOrder(temporaryOrder);
   };
@@ -22,22 +26,38 @@ export function StackCarousel() {
     const lastItem = temporaryOrder.pop();
     if (lastItem) {
       temporaryOrder.unshift(lastItem);
+      setToRight(false);
     }
     setCurrentOrder(temporaryOrder);
   };
 
+  const slideLeft = () => {
+    setToLeft(true);
+    setTimeout(() => next(), 1000);
+  };
+
+  const slideRight = () => {
+    setToRight(true);
+    setTimeout(() => prev(), 1000);
+  };
+
+  const carouselClass = classNames(styles.carousel, {
+    [styles.slideLeft]: toLeft,
+    [styles.slideRight]: toRight,
+  });
+
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.carousel}>
+        <div className={carouselClass}>
           {currentOrder.map((item) => (
             <StackItem item={item} key={`carousel_${item.label}`} />
           ))}
         </div>
       </div>
 
-      <button onClick={prev}>prev</button>
-      <button onClick={next}>next</button>
+      <button onClick={slideLeft}>slide left</button>
+      <button onClick={slideRight}>slide right</button>
     </>
   );
 }
