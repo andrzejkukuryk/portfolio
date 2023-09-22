7e617c3494baad645ace2b42de20600153b84e02
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-09-22 14:02:18 +0200

    carousel slides with mouse

--- src/scenes/home/stackCarousel.tsx
+++ src/scenes/home/stackCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { StackInfo, stack } from "../../data/stackIcons";
 import { StackItem } from "./stackItem";
 import styles from "./stackCarousel.module.scss";
@@ -9,6 +9,11 @@ export function StackCarousel() {
   const [currentOrder, setCurrentOrder] = useState<StackInfo[]>(stack);
   const [toLeft, setToLeft] = useState(false);
   const [toRight, setToRight] = useState(false);
+  const [mouseStartX, setMouseStartX] = useState(0);
+  const [mouseEndX, setMouseEndX] = useState(0);
+  const [mouseMoveX, setMouseMoveX] = useState(0);
+
+  const ref = useRef<HTMLDivElement | null>(null);
 
   const delayTime = Number(variables.carouselMove.replace("ms", ""));
 
@@ -42,6 +47,53 @@ export function StackCarousel() {
     setTimeout(() => prev(), delayTime);
   };
 
+  //@ts-ignore
+  const checkMouseStartX = (event) => {
+    event.preventDefault();
+    console.log(event.movementX);
+    setMouseStartX(event.screenX);
+  };
+
+  //@ts-ignore
+  const checkMouseEndX = (event) => {
+    setMouseEndX(event.screenX);
+  };
+
+  const checkMouseMove = () => {
+    const newMouseMove = mouseStartX - mouseEndX;
+    setMouseMoveX(newMouseMove);
+  };
+
+  useEffect(() => checkMouseMove(), [mouseEndX]);
+
+  const moveCarousel = () => {
+    if (mouseMoveX < 0) {
+      slideRight();
+    } else if (mouseMoveX > 0) {
+      slideLeft();
+    } else return;
+  };
+
+  useEffect(() => moveCarousel(), [mouseMoveX]);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener("mousedown", checkMouseStartX);
+      ref.current.addEventListener("mouseup", checkMouseEndX);
+      return () => {
+        ref.current?.removeEventListener("mousedown", checkMouseStartX);
+        ref.current?.removeEventListener("mouseup", checkMouseEndX);
+      };
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   if (ref.current) {
+  //     ref.current.addEventListener("mouseup", checkMouseEndX);
+  //     return () => ref.current?.removeEventListener("mouseup", checkMouseEndX);
+  //   }
+  // }, []);
+
   const carouselClass = classNames(styles.carousel, {
     [styles.slideLeft]: toLeft,
     [styles.slideRight]: toRight,
@@ -50,7 +102,7 @@ export function StackCarousel() {
   return (
     <>
       <div className={styles.container}>
-        <div className={carouselClass}>
+        <div ref={ref} className={carouselClass}>
           {currentOrder.map((item) => (
             <StackItem item={item} key={`carousel_${item.label}`} />
           ))}
