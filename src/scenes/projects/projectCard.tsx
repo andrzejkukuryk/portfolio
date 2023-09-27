2a1a47181e386e1857848ae03fd0845f3ab5f5fc
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-09-27 14:21:44 +0200

    projectCard mobile ready

--- src/scenes/projects/projectCard.tsx
+++ src/scenes/projects/projectCard.tsx
@@ -7,17 +7,34 @@ interface ProjectCardProps {
 }
 
 export function ProjectCard({ project }: ProjectCardProps) {
-  return (
-    <div className={styles.container}>
-      <img src={project.imgUrl} className={styles.image} alt={project.title} />
-      <div className={styles.description}>
-        <h3>{project.title}</h3>
-        <p>{project.description}</p>
-        <div className={styles.buttonsContainer}>
-          <button>Live</button>
-          <button>Repository</button>
+const handleClickLiveButton = () => {
+  window.open(project.appUrl, "_blank");
+};
+
+const handleClickRepoButton = () => {
+  window.open(project.repositoryUrl, "_blank");
+};
+
+return (
+  <div className={styles.container}>
+    <img src={project.imgUrl} className={styles.image} alt={project.title} />
+
+    <div className={styles.description}>
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+      <div className={styles.stackContainer}>
+        <h5>Stack used:</h5>
+        <div>
+          {project.stack.map((icon) => (
+            <icon.component className={styles.stackIcon} />
+          ))}
         </div>
       </div>
+      <div className={styles.buttonsContainer}>
+        <button onClick={handleClickLiveButton}>Live</button>
+        <button onClick={handleClickRepoButton}>Repository</button>
+      </div>
     </div>
-  );
+  </div>
+);
 }
