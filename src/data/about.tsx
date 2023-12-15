2853e441776344e4c25170b3b131a0281fc4e98b
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-12-15 18:43:13 +0100

    personal links

--- src/data/about.tsx
+++
@@ -1,3 +0,0 @@
-import i18n from "../i18n";
-
-export const story = i18n.t("data_about_story");
--- src/scenes/about/about.tsx
+++ src/scenes/about/about.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./about.module.scss";
 import { useNavContext } from "../../data/navProvider";
 import { usePosition } from "../../hooks/usePosition";
-import { story } from "../../data/about";
 import { PersonalLinks } from "./personalLinks";
 import { StackList } from "./stackList";
 import { useTranslation } from "react-i18next";
@@ -15,10 +14,15 @@ export function About() {
   return (
     <section ref={ref} id="about" className={styles.container}>
       <div className={styles.innerContainer}>
-        <h2>{t("about_about")}</h2>
-        <p className={styles.story}>{t("about_story")}</p>
-        <StackList />
-        <PersonalLinks />
+        <div className={styles.left}>
+          <h2>{t("about_about")}</h2>
+          <p className={styles.story}>{t("about_story_p1")}</p>
+          <p className={styles.story}>{t("about_story_p2")}</p>
+          <PersonalLinks />
+        </div>
+        <div className={styles.right}>
+          <StackList />
+        </div>
       </div>
     </section>
   );
