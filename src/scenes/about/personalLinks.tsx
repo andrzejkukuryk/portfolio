253fe50ad36f459e130d4290c19c3aafe91f3462
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2023-12-21 14:37:16 +0100

    stackList margins

--- src/scenes/about/personalLinks.tsx
+++ src/scenes/about/personalLinks.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { personalLinks } from "../../data/personalLinks";
 import { PersonalLinksItem } from "./personalLinksItem";
 import styles from "./personalLinks.module.scss";
-import { useTranslation } from "react-i18next";
 
 export function PersonalLinks() {
-  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
       <div className={styles.linksContainer}>
